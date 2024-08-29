7c398bf662d47fc063cfb9bb8a2b3b7a6b29c2d9
Ssuyu Ke <ssuyuke@asy.tw>
2024-08-29 13:27:50 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "primary-light": "#FFF7F1",
+        "secondary-light": "#FFE4C9",
+        theme: "#BC9F8B",
+        "primary-gray": "#F7F7F8",
+        "secondary-gray": "#eeeeee",
+      },
+      fontFamily: {
+        "Noto-TC": ["Noto Serif TC", "serif"],
+      },
+    },
+  },
+};
